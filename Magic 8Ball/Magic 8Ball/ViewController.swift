5d2f5c403c14fc9fa5b1d44056948ef80488e83e
Skyler Moomey <skyler_moomey-1@ou.edu.com>
2019-07-14 10:36:48 -0500

    First commit, created project in XCode

---
+++ Magic 8Ball/Magic 8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8Ball
+//
+//  Created by Skyler Moomey on 7/14/19.
+//  Copyright © 2019 Skyler Moomey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
