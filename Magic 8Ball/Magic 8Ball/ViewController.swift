09de6f6694c45194cc6f98f6f8f0ec8582e8fc7c
Skyler Moomey <skyler_moomey-1@ou.edu.com>
2019-07-14 11:00:41 -0500

    Added all images of the 8Ball to an array and created a method that calls selects one at random and changes the UIImage to it

--- Magic 8Ball/Magic 8Ball/ViewController.swift
+++ Magic 8Ball/Magic 8Ball/ViewController.swift
@@ -10,15 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let ballImages: [String] = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomInt: Int = 0;
+    
     @IBOutlet weak var eightBallImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        changeBallImages()
     }
 
     @IBAction func askButtonPressed(_ sender: Any) {
         
+        changeBallImages()
+    }
+    
+    func changeBallImages()
+    {
+        randomInt = Int.random(in: 0 ... 5)
+        
+        eightBallImage.image = UIImage(named: ballImages[randomInt])
     }
     
 }
